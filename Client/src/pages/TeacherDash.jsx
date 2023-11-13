fec3986035a033981c6c41ecb7f81485c2668fe4
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-11-13 13:50:14 +0600

    teacher dashboard done

--- Client/src/pages/TeacherDash.jsx
+++ Client/src/pages/TeacherDash.jsx
@@ -3,9 +3,10 @@ import { useUser } from "../Provider/UserProvider";
 
 const TeacherDash = () => {
     const { user } = useUser();
+    const [courseName, setCourseName] = useState(user?.courses[0]);
     const teacherId = user?.fingerprint;
-    const courseName = "VLSI"; // You can dynamically set this value based on your logic
-
+    // const courseName = "VLSI"; // You can dynamically set this value based on your logic
+    console.log(courseName)
     const [studentsData, setStudentsData] = useState([]);
     const [attendanceData, setAttendanceData] = useState({ matchedData: [], uniqueCourses: [] });
 
@@ -41,51 +42,82 @@ const TeacherDash = () => {
         };
 
         if (teacherId) {
+            setAttendanceData({ matchedData: [], uniqueCourses: [] }); // Reset attendance data when course name changes
             fetchAttendanceData();
             fetchStudentsData();
         }
-    }, [teacherId, courseName]);
+    }, [courseName]);
+
+    // ...
 
     return (
         <div className="w-[80%] mx-auto overflow-x-auto">
             <h1 className="text-5xl my-10">Attendance </h1>
             <p>Name: {user?.name}</p>
+            <p>Email: {user?.email}</p>
+            <p>Profession:{user?.category}</p>
+            <p>Mobile:{user?.mobile}</p>
             <p>Taken Course: {user?.courses.length}</p>
-
+            <div className="flex gap-2 mt-1">
+                <p>Select Course: </p>
+                <select
+                    value={courseName}
+                    onChange={(e) => setCourseName(e.target.value)}
+                >
+                    {user?.courses.map((course, index) => (
+                        <option key={index} value={course}>
+                            {course}
+                        </option>
+                    ))}
+                </select>
+            </div>
             {/* Iterate over each course */}
-            {attendanceData.uniqueCourses.map((course, index) => (
-                <table key={index} className="w-4/5 border border-collapse border-gray-400">
-                    <caption>Attendance for {course} course</caption>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Id</th>
-                            {/* Iterate over the unique dates in the attendance data for the current course */}
-                            {attendanceData.matchedData.map(data => (
-                                <th key={data.Date}>{data.Date}</th>
-                            ))}
-                            <th>Total Present Days</th>
-                            <th>Percentage</th>
-                            <th>Total marks</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {studentsData?.map(student => (
-                            <tr key={student?.id}>
-                                <td>{student?.name}</td>
-                                <td>{student?.id}</td>
+            {attendanceData.matchedData.length === 0 ? (
+                <p>No data in the server</p>
+            ) : (
+                attendanceData.uniqueCourses.map((course, index) => (
+                    <><table key={index} className="w-4/5 text-center border border-collapse border-gray-400">
+                        <caption>Attendance for {course} course</caption>
+                        <thead>
+                            <tr>
+                                <th className="border">Name</th>
+                                <th className="border">Id</th>
                                 {/* Iterate over the unique dates in the attendance data for the current course */}
-                                {Array.from(new Set(attendanceData.matchedData.filter(data => data.course === course).map(data => data.Date))).map(date => (
-                                    <td key={date}>
-                                        {attendanceData.matchedData.find(data => data.Date === date && Object.values(data).includes(student.fingerprint)) ? 'P' : 'A'}
-                                    </td>
+                                {attendanceData.matchedData.map(data => (
+                                    <th className="border" key={data.Date}>{data.Date}</th>
                                 ))}
-                                {/* Add more table cells with total present days, percentage, and total marks */}
+                                <th className="border">Total Present Days</th>
+                                <th className="border">Percentage</th>
+                                <th className="border">Total marks</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            ))}
+                        </thead>
+                        <tbody>
+                            {studentsData?.map(student => {
+                                const dates = Array.from(new Set(attendanceData.matchedData.filter(data => data.course === course).map(data => data.Date)));
+                                const presentDays = dates.filter(date => attendanceData.matchedData.find(data => data.Date === date && Object.values(data).includes(student.fingerprint))).length;
+                                const percentage = (presentDays / dates.length) * 100;
+                                return (
+                                    <tr key={student?.id}>
+                                        <td className="border">{student?.name}</td>
+                                        <td className="border">{student?.id}</td>
+                                        {/* Iterate over the unique dates in the attendance data for the current course */}
+                                        {dates.map(date => (
+                                            <td className="border" key={date}>
+                                                {attendanceData.matchedData.find(data => data.Date === date && Object.values(data).includes(student.fingerprint)) ? 'P' : 'A'}
+                                            </td>
+                                        ))}
+                                        <td className="border">{presentDays}</td>
+                                        <td className="border">{percentage.toFixed(1)}%</td>
+                                        <td className="border">{percentage / 10}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                        <button className="mt-8 p-1 bg-green-700 rounded-lg text-white">Submit</button>
+                    </>
+                ))
+            )}
         </div>
     );
 };
